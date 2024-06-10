1b45c3cf117325ea6439ca67bc0c9bc91f2307d5
Nurio34 <nurioonsoftware@gmail.com>
2024-06-10 17:48:53 +0300

    bist

--- src/Pages/5-BIST/examine.js
+++ src/Pages/5-BIST/examine.js
@@ -1,33 +1,3 @@
-//! ------------------------------
-
-//** SATIŞ ARTIŞ ORANINI İNCLE */
-//** FAVÖK ARTIŞ ORANINI İNCLE */
-//** NET KAR ARTIŞ ORANINI İNCLE */
-//** ÖZKAYNAK ARTIŞ ORANINI İNCLE */
-//** TOPLAM AKTİFLER ARTIŞ ORANINI İNCLE */
-//** HİSSE BAŞI KAR ARTIŞ ORANINI İNCLE */
-
-// her sene atış varsa yeşillendir //
-// 1 senelik, 2 senelik, 5 senelik vs. artışları gösteren select input'lar ekle
-
-//** KAR ARTIŞINA GÖRE SIRALAMA FİLTRESİ OLUŞTUR */
-
-//! ------------------------------
-
-//** BRÜT KARLILIĞA GÖRE SIRALAMA FİLTRESİ OLUŞTUR */
-//** FAVÖK KARLILIĞINA GÖRE SIRALAMA FİLTRESİ OLUŞTUR */
-//** NET KARLILIĞA GÖRE SIRALAMA FİLTRESİ OLUŞTUR */
-//** ÖZSERMAYE KARLILIĞINA GÖRE SIRALAMA FİLTRESİ OLUŞTUR */
-//** AKTİF KARLILIĞINA GÖRE SIRALAMA FİLTRESİ OLUŞTUR */
-
-//! ------------------------------
-
-//! ------------------------------
-
-//** YATIRIM PLANLARINI DATA'YA AL */
-
-// kayda değer yatırım planı olanları yeşillendir
-
 export const examine_increments = (stocks, category, year) => {
     let sales = stocks.map((obj) => {
         return {
